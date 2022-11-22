4cf9ee5eed61efc1dab4e773ec715ddd6ccfc3be
Vladislav Kucherenko <vladislav.a.kucherenko@gmail.com>
2022-11-22 18:59:31 +0300

    remove .js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const serverRoutes = require("./routes.js");
+const serverRoutes = require("./routes");
 const cors = require("cors");
 
 const PORT = 3333;
